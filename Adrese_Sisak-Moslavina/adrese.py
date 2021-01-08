323473c3ab2cf8c9e4592daf9e9c2fb6321c660a
Janjko <janjko@gmail.com>
2021-01-08 02:27:28 +0100

    Update adrese.py

--- Adrese_Sisak-Moslavina/adrese.py
+++ Adrese_Sisak-Moslavina/adrese.py
@@ -1,21 +1,19 @@
-download_url = 'http://0.0.0.0:8080/kucni_brojevi-sorted-4326.geojson'
+download_url = 'http://0.0.0.0:8080/klipane_adrese_point.geojson'
 
 no_dataset_id = True
 
 source = 'DGU'
-add_source = True
+add_source = False
 
-query = [('building',)]
+query = [('addr:housenumber',)]
 
 bbox = True
 
 max_distance = 30
 
-delete_unmatched = False
+tag_unmatched = {}
 
-tag_unmatched = False
-
-master_tags = ('addr:housenumber', 'addr:street')
+master_tags = ('addr:housenumber')
 
 def dataset(fileobj):
     import logging
@@ -27,24 +25,28 @@ def dataset(fileobj):
     for p in adrese['features']:
         if p['geometry']['type'] != 'Point':
             print('Not a point')
+            continue
         #print(str(p['geometry']['coordinates'][0])+" : "+str(p['geometry']['coordinates'][1]))
         tags = {
                 'addr:housenumber': p['properties']['KB'],
-                'addr:street': p['properties']['UL_IME'],
-                'addr:place': p['properties']['NA_IME'],
+                'source': 'DGU'
             }
+        if p['properties']['UL_IME'] == p['properties']['NA_IME']:
+            tags['addr:place'] = p['properties']['NA_IME']
+        else:
+            tags['addr:street'] = p['properties']['UL_IME']
         data.append(SourcePoint(n, float(p['geometry']['coordinates'][1]), float(p['geometry']['coordinates'][0]), tags))
         n += 1
 
-
-    
-    
     print(len(data))
     return data
 
-#def matches(osmtags, zettags):
-#    if 'gtfs:stop_id' in osmtags:
-#        return False
-#    if osmtags['name'][0] != zettags['official_name'][0]:
-#        return False
-#    return True
+def matches(osmtags, dgutags):
+    if osmtags['addr:housenumber'] != dgutags['addr:housenumber']:
+        return False
+    if ('addr:street' in osmtags) == False:
+        return False
+    if (dgutags['addr:street'].lower() in osmtags['addr:street'].lower()) == False:
+        return False
+    print ("dgu:"+dgutags['addr:street']+" "+dgutags['addr:housenumber']+"-------------- osm:"+osmtags['addr:street']+" "+osmtags['addr:housenumber'])
+    return True
